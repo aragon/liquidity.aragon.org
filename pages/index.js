bdb8e0a1fada64591d3cae000185aaf3febbbf37
Enrique Ortiz <hi@enriqueortiz.dev>
2020-08-18 09:32:35 -0400

    Semantic HTML improvements

--- pages/index.js
+++ pages/index.js
@@ -146,20 +146,30 @@ function Banner() {
         font-size: ${isCompact ? '14px' : 'auto'};
       `}
     >
-      {isCompact
-        ? 'The first ANT liquidity program ends this Friday.'
-        : 'The first ANT Liquidity Rewards program ends on Friday, August 21.'}
-      <a
-        rel="noopener noreferrer"
-        target="_blank"
-        href="https://aragon.org/blog/liquidity-rewards"
+      <p
         css={`
-          color: white;
-          text-decoration: underline;
+          text-align: ${isCompact ? 'center' : 'auto'};
+          display: inline-block;
+          align-self: center;
         `}
       >
-        &nbsp;Read more&nbsp;
-      </a>
+        {' '}
+        {isCompact
+          ? 'The first ANT liquidity program ends this Friday.'
+          : 'The first ANT Liquidity Rewards program ends on Friday, August 21.'}
+        {isCompact && <br />}&nbsp;
+        <a
+          rel="noopener noreferrer"
+          target="_blank"
+          href="https://aragon.org/blog/liquidity-rewards"
+          css={`
+            color: white;
+            text-decoration: underline;
+          `}
+        >
+          Read more
+        </a>
+      </p>
     </div>
   )
 }
