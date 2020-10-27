448c157c2fa1c2ce9a2225c656ffddd2a4174675
Andy Hook <hello@andy-hook.co.uk>
2020-10-27 15:20:00 +0000

    Temp back link

--- src/components/Pool/Pool.tsx
+++ src/components/Pool/Pool.tsx
@@ -1,12 +1,29 @@
-import React from 'react'
+import React, { useCallback } from 'react'
+import { useHistory } from 'react-router-dom'
+// @ts-ignore
+import { Link } from '@aragon/ui'
 import LayoutGutter from '../Layout/LayoutGutter'
 import LayoutLimiter from '../Layout/LayoutLimiter'
 import PoolControls from './PoolControls/PoolControls'
 
 function Pool(): JSX.Element {
+  const history = useHistory()
+
+  const handleNavigateHome = useCallback(() => {
+    history.push('/')
+  }, [history])
+
   return (
     <LayoutGutter>
       <LayoutLimiter size="small">
+        <Link
+          onClick={handleNavigateHome}
+          css={`
+            margin-bottom: 20px;
+          `}
+        >
+          Go back home
+        </Link>
         <PoolControls />
       </LayoutLimiter>
     </LayoutGutter>
