5393273992e7e42eed1cae445f1e1c15f3b11d31
Andy Hook <hello@andy-hook.co.uk>
2020-10-28 15:40:58 +0000

    Add mainnet contract addresses (#21)
    * useApprove: Remove redundant allowance reset
    
    * PoolControls: Set stake as default tab
    
    * Add mainnet contract addresses
    
    * Tidy prop order

--- src/environment/networks.ts
+++ src/environment/networks.ts
@@ -12,19 +12,19 @@ const networks = new Map<EnvNetworkName, EnvNetworkConfig>([
       ipfsGateway: 'https://ipfs.eth.aragon.network/ipfs',
       contracts: {
         unipoolAntV1: {
-          lpToken: '',
-          rewardToken: '',
-          poolContract: '',
+          poolContract: '0xEA4D68CF86BcE59Bf2bFA039B97794ce2c43dEBC',
+          lpToken: '0xfa19de406e8F5b9100E4dD5CaD8a503a6d686Efe', // UNI
+          rewardToken: '0x960b236A07cf122663c4303350609A66A7B288C0', // ANT
         },
         unipoolAntV2: {
-          lpToken: '',
-          rewardToken: '',
-          poolContract: '',
+          poolContract: '0x37b7870148b4b815cb6a4728a84816cc1150e3aa',
+          lpToken: '0x9def9511fec79f83afcbffe4776b1d817dc775ae', // UNI
+          rewardToken: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2', // WETH
         },
         balancer: {
-          lpToken: '',
-          rewardToken: '',
-          poolContract: '',
+          poolContract: '0x7f2b9e4134ba2f7e99859ae40436cbe888e86b79',
+          lpToken: '0xde0999ee4e4bea6fecb03bf4ebef2626942ec6f5', // BPT
+          rewardToken: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48', // USDC
         },
       },
     },
@@ -40,19 +40,19 @@ const networks = new Map<EnvNetworkName, EnvNetworkConfig>([
       ipfsGateway: 'https://ipfs.eth.aragon.network/ipfs',
       contracts: {
         unipoolAntV1: {
-          lpToken: '0xa117000000f279d81a1d3cc75430faa017fa5a2e',
-          rewardToken: '',
           poolContract: '0xd91c406571cd6edcdd67e3519096c868dfc160d5',
+          lpToken: '0xa117000000f279d81a1d3cc75430faa017fa5a2e',
+          rewardToken: '0xa117000000f279d81a1d3cc75430faa017fa5a2e',
         },
         unipoolAntV2: {
-          lpToken: '0xa117000000f279d81a1d3cc75430faa017fa5a2e',
-          rewardToken: '',
           poolContract: '0xd91c406571cd6edcdd67e3519096c868dfc160d5',
+          lpToken: '0xa117000000f279d81a1d3cc75430faa017fa5a2e',
+          rewardToken: '0xa117000000f279d81a1d3cc75430faa017fa5a2e',
         },
         balancer: {
-          lpToken: '0xa117000000f279d81a1d3cc75430faa017fa5a2e',
-          rewardToken: '',
           poolContract: '0xd91c406571cd6edcdd67e3519096c868dfc160d5',
+          lpToken: '0xa117000000f279d81a1d3cc75430faa017fa5a2e',
+          rewardToken: '0xa117000000f279d81a1d3cc75430faa017fa5a2e',
         },
       },
     },
