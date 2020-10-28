e2cafa4ad8ae826e89b8fb4008bbc7b0afae71d6
delfipolito <delfinapolito@gmail.com>
2020-10-28 11:54:08 -0300

    home pool buttons (#20)

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,11 +1,8 @@
-import React, { ReactNode, useCallback } from 'react'
-import { useHistory } from 'react-router-dom'
-import {
-  Link,
-  // @ts-ignore
-} from '@aragon/ui'
+import React from 'react'
 import LayoutGutter from '../Layout/LayoutGutter'
 import LayoutLimiter from '../Layout/LayoutLimiter'
+import PoolButton from './PoolButton'
+import PoolName from './PoolName'
 import {
   PATH_BALANCER_ANTV2_USDC,
   PATH_UNISWAP_ANTV2_ETH,
@@ -23,34 +20,18 @@ function Home(): JSX.Element {
             justify-content: center;
           `}
         >
-          <TempLink to={PATH_UNISWAP_ANTV2_ETH}>Uniswap (ANTv2 / ETH)</TempLink>
-          <TempLink to={PATH_BALANCER_ANTV2_USDC}>
-            Balancer (ANTv2 / USDC)
-          </TempLink>
-          <TempLink to={PATH_UNISWAP_ANT_ETH}>Uniswap (ANT / ETH)</TempLink>
+          <PoolButton to={PATH_UNISWAP_ANTV2_ETH}>
+            <PoolName name="uniswapV2" />
+          </PoolButton>
+          <PoolButton to={PATH_BALANCER_ANTV2_USDC}>
+            <PoolName name="balancer" />
+          </PoolButton>
+          <PoolButton to={PATH_UNISWAP_ANT_ETH}>
+            <PoolName name="uniswapV1" />
+          </PoolButton>
         </div>
       </LayoutLimiter>
     </LayoutGutter>
-  )
-}
-
-function TempLink({ to, children }: { to: string; children: ReactNode }) {
-  const history = useHistory()
-
-  const handleNavigate = useCallback(() => {
-    history.push(to)
-  }, [history, to])
-
-  return (
-    <Link
-      onClick={handleNavigate}
-      css={`
-        display: block;
-        padding: 20px;
-      `}
-    >
-      {children}
-    </Link>
   )
 }
 
---
+++ src/components/Home/PoolName.tsx
@@ -0,0 +1,92 @@
+import React from 'react'
+// @ts-ignore
+import { GU, useTheme, useLayout } from '@aragon/ui'
+import TokenIcon from './TokenIcon'
+import antV1 from '../../assets/token-ant-v1.svg'
+import antV2 from '../../assets/token-ant-v2.svg'
+import eth from '../../assets/token-eth.svg'
+import usdc from '../../assets/token-usdc.svg'
+
+type PoolInfo = {
+  title: string
+  tokenPair: [string, string]
+}
+type PoolNameProps = 'uniswapV2' | 'balancer' | 'uniswapV1'
+
+const POOL_INFO: Record<PoolNameProps, PoolInfo> = {
+  uniswapV2: {
+    title: 'Uniswap ANTv2 / ETH',
+    tokenPair: [antV2, eth],
+  },
+  balancer: {
+    title: 'Balancer ANTv2 / USDC',
+    tokenPair: [antV2, usdc],
+  },
+  uniswapV1: {
+    title: 'Uniswap ANT / ETH',
+    tokenPair: [antV1, eth],
+  },
+}
+
+function PoolName({ name }: { name: PoolNameProps }): JSX.Element {
+  const theme = useTheme()
+  const { layoutName } = useLayout()
+  const compactMode = layoutName === 'small'
+
+  const { title, tokenPair } = POOL_INFO[name]
+  const [firstTokenLogo, secondTokenLogo] = tokenPair
+
+  return (
+    <div
+      css={`
+        display: flex;
+        align-items: center;
+        padding: ${0.5 * GU}px 0;
+        justify-content: ${compactMode ? 'center' : 'inherit'};
+      `}
+    >
+      <div
+        css={`
+          display: flex;
+          flex-direction: ${compactMode ? 'column' : 'row'};
+          align-items: center;
+          text-decoration: none;
+          white-space: initial;
+          text-align: ${compactMode ? 'center' : 'left'};
+        `}
+      >
+        <div
+          css={`
+            display: flex;
+            flex-shrink: 0;
+            padding: ${0.5 * GU}px;
+          `}
+        >
+          <TokenIcon
+            logo={firstTokenLogo}
+            css={`
+              z-index: 1;
+            `}
+          />
+          <TokenIcon
+            logo={secondTokenLogo}
+            css={`
+              margin-left: -${1 * GU}px;
+            `}
+          />
+        </div>
+        <span
+          css={`
+            margin-left: ${compactMode ? 2 * GU : 0}px;
+            font-size: ${3 * GU}px;
+            color: ${theme.content};
+          `}
+        >
+          {title}
+        </span>
+      </div>
+    </div>
+  )
+}
+
+export default PoolName
